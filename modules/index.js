9a13d864938402d603ec336d4e95349fe01bfc06
Shahzaib <shahzaibali1022@yahoo.com>
2022-09-02 21:46:58 +0500

    ar devices screen added

--- modules/index.js
+++ modules/index.js
@@ -2,7 +2,7 @@ import mods from "./*/index.js";
 import { getModules } from "./modules.js";
 
 export const modules = getModules(mods);
-export const initialRoute = "screenAlert";
+export const initialRoute = "screenArDevices";
 // export const initialRoute = modules[0].value.title;
 export const slices = modules
   .filter(mod => mod.value.slice)
