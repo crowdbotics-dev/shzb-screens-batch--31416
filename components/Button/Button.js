1fe6277f5ef064aca3da806cd4b125b14b861c40
Shahzaib <shahzaibali1022@yahoo.com>
2022-09-02 20:56:45 +0500

    button updated

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -31,6 +31,12 @@ const buttonStyles = StyleSheet.create({
   },
   shadowContainer: {
     shadowColor: "rgba(0, 0, 0, 0.5)",
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
     elevation: 10,
     backgroundColor: "#fff",
     borderRadius: 10
